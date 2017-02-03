ef5ff2e2687cc9924385f0aac422e8e3241ecfe6
Walmor Felicio da Silva <walmor.felicio@DCX.UFPB.BR>
2017-02-03 12:39:56 -0300

    programa refecao

---
+++ refeição.py
@@ -0,0 +1,7 @@
+custoRefeição = 44.5
+imposto = 0.0675
+gorjeta = 0.15
+custoTotal= custoRefeição+(custoRefeição*imposto)
+gorjeta= (custoTotal*gorjeta)
+print("gorjeta %.2f" % gorjeta )
+                 
